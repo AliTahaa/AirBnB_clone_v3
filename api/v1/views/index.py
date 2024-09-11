24906cddc6b4a925b1208878ef2fc61fe585db2d
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-09-11 12:41:53 +0300

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,3 +14,14 @@ def api_status():
     """ Return status """
     response = {"status": "OK"}
     return jsonify(response)
+
+@app_views.route('/stats')
+def api_stats():
+    """ Return stats """
+    from models import storage
+    classes = {"Amenity": "amenities", "City": "cities", "Place": "places",
+               "Review": "reviews", "State": "states", "User": "users"}
+    response = {}
+    for key, value in classes.items():
+        response[value] = storage.count(key)
+    return jsonify(response)
