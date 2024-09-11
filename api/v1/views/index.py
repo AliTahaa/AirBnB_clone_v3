35a65b316da45d571e361d972e1deba0f5582417
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-09-11 12:24:51 +0300

    task 3

---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Blueprint for API
+"""
+
+from api.v1.views import app_views
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def api_status():
+    return jsonify({"status": "OK"})
