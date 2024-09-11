b828f8e2a7f96260d3a7d44c1fa700d178876e60
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-09-11 12:37:57 +0300

    fix task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,5 +18,5 @@ def teardown(exception):
 
 if __name__ == "__main__":
     HOST = getenv("HBNB_API_HOST", "0.0.0.0")
-    PORT = getenv("HBNB_API_PORT", 5000) 
+    PORT = getenv("HBNB_API_PORT", 5000)
     app.run(host=HOST, port=PORT, threaded=True)
