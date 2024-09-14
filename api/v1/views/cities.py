041596f7a165a36cb1bc8f6e8e565fe2e84b2257
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-09-14 14:38:06 +0300

    task 7

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -38,7 +38,8 @@ def delete_city(city_id):
     return jsonify({})
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """ Create a city """
     state = storage.get("State", state_id)
