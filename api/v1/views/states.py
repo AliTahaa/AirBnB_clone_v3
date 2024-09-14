be33ab222d759d8e4a892900cb3f824c8aad8904
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-09-14 14:28:02 +0300

    fix task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -44,6 +44,8 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """ Create a state """
+    if not request.is_json:
+        return jsonify({"error": "Not a JSON"}), 400
     state = request.get_json()
     if state is None:
         return jsonify({"error": "Not a JSON"}), 400
