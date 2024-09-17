099bf14f58483588162a5b3227790fe74172194b
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-09-17 14:04:29 +0300

    task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,11 @@ from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
